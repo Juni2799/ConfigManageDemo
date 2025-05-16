e2a1b5be7f5c41d5c369bbd5dfa2b3230602022f
Juni2799 <akshaankhanna@gmail.com>
2025-05-16 15:50:16 +0530

    Added changes to use config properties from a common class

--- src/main/java/com/info/config_demo/controller/ConfigController.java
+++ src/main/java/com/info/config_demo/controller/ConfigController.java
@@ -1,5 +1,7 @@
 package com.info.config_demo.controller;
 
+import com.info.config_demo.config.BuildInfo;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,18 +9,25 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/build-info")
+@RequiredArgsConstructor
 public class ConfigController {
-    @Value("${build.id:1}")
-    private String buildId;
+//    @Value("${build.id:1}")
+//    private String buildId;
+//
+//    @Value("${build.version:1.1}")
+//    private String buildVersion;
+//
+//    @Value("${build.info:Default-build}")
+//    private String buildInfo;
 
-    @Value("${build.version:1.1}")
-    private String buildVersion;
+    private final BuildInfo buildInfo;
 
-    @Value("${build.info:Default-build}")
-    private String buildInfo;
+    @Value("${main-build.id}")
+    private String mainBuildInfo;
 
     @GetMapping
     public String getBuildInfo(){
-        return "Build Id: " + buildId + ", Build Version: " + buildVersion + ", Build Info: " + buildInfo;
+        return "Build Id: " + buildInfo.getId() + ", Build Version: " + buildInfo.getVersion() +
+                ", Build Info: " + buildInfo.getInfo() + ", Main-Build id: " + mainBuildInfo;
     }
 }
